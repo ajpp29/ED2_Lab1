093a76d31e5954318565708fccf57d8dde440cf9
Pablohe18 <31459420+Pablohe18@users.noreply.github.com>
2019-08-31 22:37:58 -0600

    Lectura de caracteres
    Creacion de objeto Nodohuffman con sus atributos y metodos de calculo de frecuencias por caracter

---
+++ ED2_Lab1/ED2_Lab1/Controllers/HuffmanController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ED2_Lab1.Models;
+
+namespace ED2_Lab1.Controllers
+{
+    public class HuffmanController : Controller
+    {
+        // GET: Huffman
+        public ActionResult Index()
+        {
+            List<HuffmanNode> ListaNodos = new List<HuffmanNode>();
+            ListaNodos = ObtenerLista();
+            return View(ListaNodos);
+        }
+        public List<HuffmanNode> ObtenerLista()
+        {
+            List<HuffmanNode> nodeList = new List<HuffmanNode>(); 
+            try
+            {
+               
+                FileStream stream = new FileStream("C://Users//carlo//Desktop//cerote.txt", FileMode.Open, FileAccess.Read);
+                for (int i = 0; i < stream.Length; i++)
+                {
+                    string read = Convert.ToChar(stream.ReadByte()).ToString();
+                    if (nodeList.Exists(x => x.caracter == read)) 
+                        nodeList[nodeList.FindIndex(y => y.caracter == read)].Frecuencia(); 
+                    else
+                        nodeList.Add(new HuffmanNode(read));   
+                }
+                nodeList.Sort();   
+                return nodeList;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+
+
+}
+
+
+
+
+//        public List<HuffmanNode> ObtenerListaNodos()
+//        {
+//            var buffer = new byte[bufferLength];
+//            using (var file = new FileStream(@"C:\hola.txt", FileMode.Open))
+//            {
+//                using (var reader = new BinaryReader(file))
+//                {
+//                    while (reader.BaseStream.Position != reader.BaseStream.Length)
+//                    {
+//                        buffer = reader.ReadBytes(bufferLength);
+//                        if (ListaNodos.Exists(x => x.caracter == buffer.ToString()))
+//                            ListaNodos[ListaNodos.FindIndex(y => y.caracter == buffer.ToString())].Frecuencia() ;
+//                        else
+//                            ListaNodos.Add(new HuffmanNode(buffer.ToString()));
+//                    }
+//                    ListaNodos.Sort();
+//                    return ListaNodos;
+//                }
+
+//            }
+//        }
---
+++ ED2_Lab1/ED2_Lab1/Models/HuffmanNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ED2_Lab1.Models
+{
+    public class HuffmanNode
+    {
+        public string caracter;   
+        public int frecuencia;         
+        public string code;           
+        public HuffmanNode parentNode; 
+        public HuffmanNode leftTree;   
+        public HuffmanNode rightTree; 
+        public bool isLeaf;           
+
+
+        public HuffmanNode(string value)   
+        {
+            caracter = value;     
+            frecuencia = 1;      
+
+            rightTree = leftTree = parentNode = null;      
+
+            code = "";         
+            isLeaf = true;     
+        }
+
+        public void Frecuencia()             
+        {
+            frecuencia++;
+        }
+    }
+}
