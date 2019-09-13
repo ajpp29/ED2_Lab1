6d8848b8c5355611721d5d42b32fd4aa99684bf7
Angel Jimenez <angeljpp29@gmail.com>
2019-09-13 11:02:29 -0600

    Modificacion de la clase Log
    Se implemento una funcion que retorne una lista con el historial de compresion apartir del archivo Log

--- ED2_Lab1/ED2_Lab1/Compresion/LogFile.cs
+++ ED2_Lab1/ED2_Lab1/Compresion/LogFile.cs
@@ -3,29 +3,72 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
+using ED2_Lab1.Models;
 
 namespace ED2_Lab1.Compresion
 {
     public class LogFile
     {
+        public List<Archivo> ListaHistorial;
+
         public LogFile()
         {
-
+            ListaHistorial = new List<Archivo>();
         }
 
-        public void GenerarLog(string nombre_original,int razon_compresion, int factor_compresion,int porcentaje_reduccion)
+        public void GenerarLog(string nombre_original,long tamanio_original, long tamanio_compreso)
         {
-            //int razon_compresion;
-            //int factor_compresion;
-            //int porcentaje_reduccion;
+            int razon_compresion = default(int);
+            int factor_compresion = default(int);
+            int porcentaje_reduccion = default(int);
+            DateTime dateTime = DateTime.Today;
+
+            long tamanio_aux = tamanio_compreso / tamanio_original;
+            long reduccion_aux = (Math.Abs(tamanio_original - tamanio_compreso) / tamanio_original);
+
+            razon_compresion = unchecked((int)tamanio_aux);
+            factor_compresion = 1 / razon_compresion;
+            porcentaje_reduccion = unchecked((int)reduccion_aux);
 
             string filepath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             filepath = filepath + @"\Log.txt";
 
             StreamWriter escribir = File.AppendText(filepath);
-            escribir.WriteLine("Nombre:" + nombre_original + ";Razon:" + razon_compresion + ";Factor:" + factor_compresion + ";Porcentaje de reduccion:" + porcentaje_reduccion);
+            escribir.WriteLine("Fecha Compresion," + dateTime.ToString("dd/MM/yyyy HH:mm:ss") + ";Nombre," + nombre_original + ";Razon," + razon_compresion + ";Factor," + factor_compresion + ";Porcentaje de reduccion," + porcentaje_reduccion);
             escribir.Close();
 
+        }
+
+        public List<Archivo> ObtenerHistorial()
+        {
+            ListaHistorial = new List<Archivo>();
+
+            string filepath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            filepath = filepath + @"\Log.txt";
+
+            StreamReader leer = new StreamReader(filepath);
+
+            string cadena = leer.ReadLine();
+            string[] splitCadena = new string[5];
+
+            while (cadena != null)
+            {
+                Archivo aAux = new Archivo();
+                splitCadena = cadena.Split(';');
+                aAux.fechaCompresion = DateTime.ParseExact(splitCadena[0].Split(',')[1].Trim(), "dd/MM/yyyy HH:mm:ss", null);
+                aAux.nombreArchivo = splitCadena[1].Split(',')[1].Trim();
+                aAux.razon_compresion = int.Parse(splitCadena[2].Split(',')[1].Trim());
+                aAux.factor_compresion = int.Parse(splitCadena[3].Split(',')[1].Trim());
+                aAux.porcentaje_reduccion = int.Parse(splitCadena[4].Split(',')[1].Trim());
+
+                ListaHistorial.Add(aAux);
+
+                cadena = leer.ReadLine();
+            }
+
+            ListaHistorial.Sort();
+
+            return ListaHistorial;
         }
     }
 }
---
+++ ED2_Lab1/ED2_Lab1/Models/Archivo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ED2_Lab1.Models
+{
+    public class Archivo : IComparable<Archivo>
+    {
+        public DateTime fechaCompresion;
+        public string nombreArchivo;
+        public int razon_compresion;
+        public int factor_compresion;
+        public int porcentaje_reduccion;
+
+        public Archivo()
+        {
+            fechaCompresion = new DateTime();
+            nombreArchivo = default(string);
+            razon_compresion = default(int);
+            factor_compresion = default(int);
+            porcentaje_reduccion = default(int);
+
+        }
+
+        public int CompareTo(Archivo other)
+        {
+            return this.fechaCompresion.CompareTo(other.fechaCompresion);
+        }
+    }
+}
