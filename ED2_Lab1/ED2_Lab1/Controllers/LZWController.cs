428851a291d250f5ec53517dcc475ca4a5e3fc36
Pablohe18 <31459420+Pablohe18@users.noreply.github.com>
2019-09-22 15:46:59 -0600

    METODO COMPRESIO/DESCOMPRESION LZW

---
+++ ED2_Lab1/ED2_Lab1/Controllers/LZWController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ED2_Lab1.Models;
+using System.Web.Mvc;
+
+namespace ED2_Lab1.Controllers
+{
+    public class LZWController : Controller
+    {
+        LZW algoritmo = new LZW();
+        public ActionResult Index()
+        {
+            List<int> compreso = algoritmo.Compresion("HOLACOMOESTASMAJE");
+            string descompreso = algoritmo.Descompresion(compreso);
+            return View();
+        }
+    }
+}
---
+++ ED2_Lab1/ED2_Lab1/Models/LZW.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace ED2_Lab1.Models
+{
+    public class LZW
+    {
+        public  List<int> Compresion(string Descomprimido)
+        {
+            // Crando Diccionario
+            Dictionary<string, int> Diccionario = new Dictionary<string, int>();
+            for (int i = 0; i < 256; i++)
+                Diccionario.Add(((char)i).ToString(), i);
+
+            string w = string.Empty;
+            List<int> ArchivoComp = new List<int>();
+
+            foreach (char c in Descomprimido)
+            {
+                string wc = w + c;
+                if (Diccionario.ContainsKey(wc))
+                {
+                    w = wc;
+                }
+                else
+                {
+                    ArchivoComp.Add(Diccionario[w]);
+                    Diccionario.Add(wc, Diccionario.Count);
+                    w = c.ToString();
+                }
+            }
+            if (!string.IsNullOrEmpty(w))
+                ArchivoComp.Add(Diccionario[w]);
+
+            return ArchivoComp;
+        }
+
+        public string Descompresion(List<int> Comprimido)
+        {
+            // Crando Diccionario
+            Dictionary<int, string> Diccionario = new Dictionary<int, string>();
+            for (int i = 0; i < 256; i++)
+                Diccionario.Add(i, ((char)i).ToString());
+
+            string w = Diccionario[Comprimido[0]];
+            Comprimido.RemoveAt(0);
+            StringBuilder Descomprimido = new StringBuilder(w);
+
+            foreach (int k in Comprimido)
+            {
+                string entry = null;
+                if (Diccionario.ContainsKey(k))
+                    entry = Diccionario[k];
+                else if (k == Diccionario.Count)
+                    entry = w + w[0];
+
+                Descomprimido.Append(entry);
+
+                // Agregando al diccionario la entrada
+                Diccionario.Add(Diccionario.Count, w + entry[0]);
+                w = entry;
+            }
+
+            return Descomprimido.ToString();
+        }
+    }
+}
